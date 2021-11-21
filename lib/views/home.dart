511dce3e7f45f72fdbdc5976204dc1862c32879f
RishiAhuja <www.rishiahuja@gmail.com>
2021-11-21 14:26:43 +0530

    loader-commit-changer

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:nationality/views/result.dart';
 import 'package:sizer/sizer.dart';
 import 'package:http/http.dart' as http;
@@ -15,28 +16,113 @@ class Home extends StatefulWidget {
 }
 
 bool _isLoading = false;
+
 class RandomImage extends StatelessWidget {
   final int random;
   RandomImage({this.random});
 
   @override
   Widget build(BuildContext context) {
-    if(random == 0){
-      return SizerUtil.orientation == Orientation.portrait ? Image.asset('assets/home/1.png',) : Align(alignment: Alignment.center,child: Image.asset('assets/home/1.png', width: 25.h, height: 25.h,));
-    }if(random == 1){
-      return SizerUtil.orientation == Orientation.portrait ? Image.asset('assets/home/2.png',) : Align(alignment: Alignment.center,child: Image.asset('assets/home/2.png', width: 25.h, height: 25.h,));
-    }if(random == 2){
-      return SizerUtil.orientation == Orientation.portrait ? Image.asset('assets/home/3.png',) : Align(alignment: Alignment.center,child: Image.asset('assets/home/3.png', width: 25.h, height: 25.h,));
-    }if(random == 3){
-      return SizerUtil.orientation == Orientation.portrait ? Image.asset('assets/home/4.png',) : Align(alignment: Alignment.center,child: Image.asset('assets/home/4.png', width: 25.h, height: 25.h,));
-    }if(random == 4){
-      return SizerUtil.orientation == Orientation.portrait ? Image.asset('assets/home/5.png',) : Align(alignment: Alignment.center,child: Image.asset('assets/home/5.png', width: 30.h, height: 30.h,));
+    if (random == 0) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Image.asset(
+              'assets/home/1.png',
+            )
+          : Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/home/1.png',
+                width: 25.h,
+                height: 25.h,
+              ));
+    }
+    if (random == 1) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Image.asset(
+              'assets/home/2.png',
+            )
+          : Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/home/2.png',
+                width: 25.h,
+                height: 25.h,
+              ));
+    }
+    if (random == 2) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Image.asset(
+              'assets/home/3.png',
+            )
+          : Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/home/3.png',
+                width: 25.h,
+                height: 25.h,
+              ));
+    }
+    if (random == 3) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Image.asset(
+              'assets/home/4.png',
+            )
+          : Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/home/4.png',
+                width: 25.h,
+                height: 25.h,
+              ));
+    }
+    if (random == 4) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Image.asset(
+              'assets/home/5.png',
+            )
+          : Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/home/5.png',
+                width: 30.h,
+                height: 30.h,
+              ));
+    }
+    if (random == 5) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Lottie.asset('assets/home/l1.json', animate: true, repeat: true)
+          : Align(
+              alignment: Alignment.center,
+              child: Lottie.asset(
+                'assets/home/l1.json',
+                animate: true,
+                repeat: true,
+                width: 30.h,
+                height: 30.h,
+              ));
+    }
+    if (random == 6) {
+      return SizerUtil.orientation == Orientation.portrait
+          ? Lottie.asset('assets/home/l1.json', animate: true, repeat: true)
+          : Align(
+              alignment: Alignment.center,
+              child: Lottie.asset(
+                'assets/home/l1.json',
+                animate: true,
+                repeat: true,
+                width: 30.h,
+                height: 30.h,
+              ));
     }
   }
 }
 
 int _random = 0;
 var info;
+var countryInfo;
+
+
+
 class _HomeState extends State<Home> {
   final TextEditingController controller = TextEditingController();
   @override
@@ -45,146 +131,173 @@ class _HomeState extends State<Home> {
     super.initState();
     var rng = new Random();
     setState(() {
-      _random = rng.nextInt(5);
+      _random = rng.nextInt(7);
       print(_random);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: AppBar(
-            leading: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 14),
-              child: Image.asset('assets/home/india.png', width: 60, height: 60,),
+          backgroundColor: Colors.white,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: AppBar(
+              leading: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 14),
+                child: Image.asset(
+                  'assets/home/india.png',
+                  width: 60,
+                  height: 60,
+                ),
+              ),
+              elevation: 0,
+              backgroundColor: Colors.white,
             ),
-            elevation: 0,
-            backgroundColor: Colors.white,
           ),
-        ),
-        body: _isLoading ? Center(child: CircularProgressIndicator()) : Container(
-          child: ListView(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text("Let's test Nationality", style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.bold
-                  ),),
-                ),
-              ),
-              RandomImage(random: _random),
-              const SizedBox(height: 30,),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(color: Colors.grey[400], width: 1)
-                ),
-                margin: const EdgeInsets.symmetric(horizontal: 25),
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: TextFormField(
-                  controller: controller,
-                  style: GoogleFonts.poppins(),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Type someone's name..",
-                      hintStyle: GoogleFonts.poppins(
-                          color: Colors.grey
-                      )
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: GestureDetector(
-                  onTap: () async{
-                    setState(() {
-                      _isLoading = true;
-                    });
-                    var req = await http.get(Uri.parse('https://api.nationalize.io/?name=${controller.text}'));
-                    setState(() {
-                      info = json.decode(req.body);
-                      _isLoading = false;
-                    });
-                    print(info['name']);
-                    print(info['country']);
-                    info['country'].forEach((element) {
-                      print(element);
-                      print(element['country_id']);
-                      print(element['probability']);
-                    });
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Result(data: info,)));
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width/1.3,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        gradient: LinearGradient(
-                            colors: [
-                              Colors.red[300],
-                              Colors.red[400],
-                              Colors.red,
-                              Colors.red[600],
-                              Colors.red[700],
-                            ]
-                        )
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Test Nationality',
-                        style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20,),
-              GestureDetector(
-                onTap: () async{
-                  await launch('https://rishiahuja.github.io/my-portfolio');
-                },
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+          body: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Container(
+                  child: ListView(
                     children: [
-                      Text(
-                        'Developed by ',
-                        style: GoogleFonts.poppins(
-                          fontSize: 10.sp,
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            "Let's test Nationality",
+                            style: GoogleFonts.poppins(
+                                color: Colors.black,
+                                fontSize: 20.sp,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
-                      Text(
-                        'Rishi',
-                        style: GoogleFonts.poppins(
-                          fontSize: 10.sp,
-                          decoration: TextDecoration.underline
+                      RandomImage(random: _random),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(15),
+                            border:
+                                Border.all(color: Colors.grey[400], width: 1)),
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: TextFormField(
+                          controller: controller,
+                          style: GoogleFonts.poppins(),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Type someone's first name..",
+                              hintStyle:
+                                  GoogleFonts.poppins(color: Colors.grey)),
                         ),
                       ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        child: GestureDetector(
+                          onTap: () async {
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            var req = await http.get(Uri.parse(
+                                'https://api.nationalize.io/?name=${controller.text}'));
+
+                            setState(() {
+                              info = json.decode(req.body);
+                              _isLoading = false;
+                            });
+                            print(info['name']);
+                            print(info['country']);
+                            info['country'].forEach((element) {
+                              print(element);
+                              print(element['country_id']);
+                              print(element['probability']);
+                            });
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Result(
+                                          data: info,
+                                        )));
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width / 1.3,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                gradient: LinearGradient(colors: [
+                                  Colors.red[300],
+                                  Colors.red[400],
+                                  Colors.red,
+                                  Colors.red[600],
+                                  Colors.red[700],
+                                ])),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Test Nationality',
+                                style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 16.sp,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Only enter your First name',
+                          style: GoogleFonts.poppins(
+                            fontSize: 10.sp,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          await launch(
+                              'https://rishiahuja.github.io/my-portfolio');
+                        },
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Developed by ',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 10.sp,
+                                ),
+                              ),
+                              Text(
+                                'Rishi',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 10.sp,
+                                    decoration: TextDecoration.underline),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                ),
-              )
-            ],
-          ),
-        )
-      ),
+                )),
     );
   }
 }
-
-
--- lib/views/result.dart
+++ lib/views/result.dart
@@ -1,6 +1,6 @@
-import 'package:country_provider/country_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:nationality/views/home.dart';
 import 'package:sizer/sizer.dart';
 
@@ -14,6 +14,259 @@ class Result extends StatefulWidget {
   _ResultState createState() => _ResultState();
 }
 
+Map countryMap = {
+  "BD": "Bangladesh",
+  "BE": "Belgium",
+  "BF": "Burkina Faso",
+  "BG": "Bulgaria",
+  "BA": "Bosnia and Herzegovina",
+  "BB": "Barbados",
+  "WF": "Wallis and Futuna",
+  "BL": "Saint Barthelemy",
+  "BM": "Bermuda",
+  "BN": "Brunei",
+  "BO": "Bolivia",
+  "BH": "Bahrain",
+  "BI": "Burundi",
+  "BJ": "Benin",
+  "BT": "Bhutan",
+  "JM": "Jamaica",
+  "BV": "Bouvet Island",
+  "BW": "Botswana",
+  "WS": "Samoa",
+  "BQ": "Bonaire, Saint Eustatius and Saba ",
+  "BR": "Brazil",
+  "BS": "Bahamas",
+  "JE": "Jersey",
+  "BY": "Belarus",
+  "BZ": "Belize",
+  "RU": "Russia",
+  "RW": "Rwanda",
+  "RS": "Serbia",
+  "TL": "East Timor",
+  "RE": "Reunion",
+  "TM": "Turkmenistan",
+  "TJ": "Tajikistan",
+  "RO": "Romania",
+  "TK": "Tokelau",
+  "GW": "Guinea-Bissau",
+  "GU": "Guam",
+  "GT": "Guatemala",
+  "GS": "South Georgia and the South Sandwich Islands",
+  "GR": "Greece",
+  "GQ": "Equatorial Guinea",
+  "GP": "Guadeloupe",
+  "JP": "Japan",
+  "GY": "Guyana",
+  "GG": "Guernsey",
+  "GF": "French Guiana",
+  "GE": "Georgia",
+  "GD": "Grenada",
+  "GB": "United Kingdom",
+  "GA": "Gabon",
+  "SV": "El Salvador",
+  "GN": "Guinea",
+  "GM": "Gambia",
+  "GL": "Greenland",
+  "GI": "Gibraltar",
+  "GH": "Ghana",
+  "OM": "Oman",
+  "TN": "Tunisia",
+  "JO": "Jordan",
+  "HR": "Croatia",
+  "HT": "Haiti",
+  "HU": "Hungary",
+  "HK": "Hong Kong",
+  "HN": "Honduras",
+  "HM": "Heard Island and McDonald Islands",
+  "VE": "Venezuela",
+  "PR": "Puerto Rico",
+  "PS": "Palestinian Territory",
+  "PW": "Palau",
+  "PT": "Portugal",
+  "SJ": "Svalbard and Jan Mayen",
+  "PY": "Paraguay",
+  "IQ": "Iraq",
+  "PA": "Panama",
+  "PF": "French Polynesia",
+  "PG": "Papua New Guinea",
+  "PE": "Peru",
+  "PK": "Pakistan",
+  "PH": "Philippines",
+  "PN": "Pitcairn",
+  "PL": "Poland",
+  "PM": "Saint Pierre and Miquelon",
+  "ZM": "Zambia",
+  "EH": "Western Sahara",
+  "EE": "Estonia",
+  "EG": "Egypt",
+  "ZA": "South Africa",
+  "EC": "Ecuador",
+  "IT": "Italy",
+  "VN": "Vietnam",
+  "SB": "Solomon Islands",
+  "ET": "Ethiopia",
+  "SO": "Somalia",
+  "ZW": "Zimbabwe",
+  "SA": "Saudi Arabia",
+  "ES": "Spain",
+  "ER": "Eritrea",
+  "ME": "Montenegro",
+  "MD": "Moldova",
+  "MG": "Madagascar",
+  "MF": "Saint Martin",
+  "MA": "Morocco",
+  "MC": "Monaco",
+  "UZ": "Uzbekistan",
+  "MM": "Myanmar",
+  "ML": "Mali",
+  "MO": "Macao",
+  "MN": "Mongolia",
+  "MH": "Marshall Islands",
+  "MK": "Macedonia",
+  "MU": "Mauritius",
+  "MT": "Malta",
+  "MW": "Malawi",
+  "MV": "Maldives",
+  "MQ": "Martinique",
+  "MP": "Northern Mariana Islands",
+  "MS": "Montserrat",
+  "MR": "Mauritania",
+  "IM": "Isle of Man",
+  "UG": "Uganda",
+  "TZ": "Tanzania",
+  "MY": "Malaysia",
+  "MX": "Mexico",
+  "IL": "Israel",
+  "FR": "France",
+  "IO": "British Indian Ocean Territory",
+  "SH": "Saint Helena",
+  "FI": "Finland",
+  "FJ": "Fiji",
+  "FK": "Falkland Islands",
+  "FM": "Micronesia",
+  "FO": "Faroe Islands",
+  "NI": "Nicaragua",
+  "NL": "Netherlands",
+  "NO": "Norway",
+  "NA": "Namibia",
+  "VU": "Vanuatu",
+  "NC": "New Caledonia",
+  "NE": "Niger",
+  "NF": "Norfolk Island",
+  "NG": "Nigeria",
+  "NZ": "New Zealand",
+  "NP": "Nepal",
+  "NR": "Nauru",
+  "NU": "Niue",
+  "CK": "Cook Islands",
+  "XK": "Kosovo",
+  "CI": "Ivory Coast",
+  "CH": "Switzerland",
+  "CO": "Colombia",
+  "CN": "China",
+  "CM": "Cameroon",
+  "CL": "Chile",
+  "CC": "Cocos Islands",
+  "CA": "Canada",
+  "CG": "Republic of the Congo",
+  "CF": "Central African Republic",
+  "CD": "Democratic Republic of the Congo",
+  "CZ": "Czech Republic",
+  "CY": "Cyprus",
+  "CX": "Christmas Island",
+  "CR": "Costa Rica",
+  "CW": "Curacao",
+  "CV": "Cape Verde",
+  "CU": "Cuba",
+  "SZ": "Swaziland",
+  "SY": "Syria",
+  "SX": "Sint Maarten",
+  "KG": "Kyrgyzstan",
+  "KE": "Kenya",
+  "SS": "South Sudan",
+  "SR": "Suriname",
+  "KI": "Kiribati",
+  "KH": "Cambodia",
+  "KN": "Saint Kitts and Nevis",
+  "KM": "Comoros",
+  "ST": "Sao Tome and Principe",
+  "SK": "Slovakia",
+  "KR": "South Korea",
+  "SI": "Slovenia",
+  "KP": "North Korea",
+  "KW": "Kuwait",
+  "SN": "Senegal",
+  "SM": "San Marino",
+  "SL": "Sierra Leone",
+  "SC": "Seychelles",
+  "KZ": "Kazakhstan",
+  "KY": "Cayman Islands",
+  "SG": "Singapore",
+  "SE": "Sweden",
+  "SD": "Sudan",
+  "DO": "Dominican Republic",
+  "DM": "Dominica",
+  "DJ": "Djibouti",
+  "DK": "Denmark",
+  "VG": "British Virgin Islands",
+  "DE": "Germany",
+  "YE": "Yemen",
+  "DZ": "Algeria",
+  "US": "United States",
+  "UY": "Uruguay",
+  "YT": "Mayotte",
+  "UM": "United States Minor Outlying Islands",
+  "LB": "Lebanon",
+  "LC": "Saint Lucia",
+  "LA": "Laos",
+  "TV": "Tuvalu",
+  "TW": "Taiwan",
+  "TT": "Trinidad and Tobago",
+  "TR": "Turkey",
+  "LK": "Sri Lanka",
+  "LI": "Liechtenstein",
+  "LV": "Latvia",
+  "TO": "Tonga",
+  "LT": "Lithuania",
+  "LU": "Luxembourg",
+  "LR": "Liberia",
+  "LS": "Lesotho",
+  "TH": "Thailand",
+  "TF": "French Southern Territories",
+  "TG": "Togo",
+  "TD": "Chad",
+  "TC": "Turks and Caicos Islands",
+  "LY": "Libya",
+  "VA": "Vatican",
+  "VC": "Saint Vincent and the Grenadines",
+  "AE": "United Arab Emirates",
+  "AD": "Andorra",
+  "AG": "Antigua and Barbuda",
+  "AF": "Afghanistan",
+  "AI": "Anguilla",
+  "VI": "U.S. Virgin Islands",
+  "IS": "Iceland",
+  "IR": "Iran",
+  "AM": "Armenia",
+  "AL": "Albania",
+  "AO": "Angola",
+  "AQ": "Antarctica",
+  "AS": "American Samoa",
+  "AR": "Argentina",
+  "AU": "Australia",
+  "AT": "Austria",
+  "AW": "Aruba",
+  "IN": "India",
+  "AX": "Aland Islands",
+  "AZ": "Azerbaijan",
+  "IE": "Ireland",
+  "ID": "Indonesia",
+  "UA": "Ukraine",
+  "QA": "Qatar",
+  "MZ": "Mozambique"
+};
+
 class _ResultState extends State<Result> {
   var info;
   List country = [];
@@ -26,6 +279,7 @@ class _ResultState extends State<Result> {
     super.initState();
   }
   getData() async{
+
     setState(() {
       info = widget.data;
       info['country'].forEach((element) {
@@ -33,16 +287,9 @@ class _ResultState extends State<Result> {
         country.add(element['country_id']);
         print(element['probability']);
         probability.add(element['probability']);
+        countryName.add(countryMap[element['country_id']]);
       });
     });
-    for(int i = 0; i <= (country.length - 1); i++){
-      Country result = await CountryProvider.getCountryByCode("${country[i].toString().toLowerCase()}");
-      print(result.name);
-      setState(() {
-        countryName.add(result.name);
-
-      });
-    }
   }
   @override
   Widget build(BuildContext context) {
@@ -56,7 +303,78 @@ class _ResultState extends State<Result> {
             backgroundColor: Colors.white,
           ),
         ),
-        body: Container(
+        body: countryName.isEmpty ? Container(
+          child: ListView(
+            children: [
+              Lottie.asset('assets/home/l2.json', animate: true, repeat: true) ,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Please enter other names, AI can't predict your name",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 12.sp,
+                      color: Colors.grey
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: GestureDetector(
+                  onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home())),
+                  child: Container(
+
+                    width: MediaQuery.of(context).size.width/1.3,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.red[300],
+                            Colors.red[400],
+                            Colors.red,
+                            Colors.red[600],
+                            Colors.red[700],
+                          ]
+                      ),
+
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Check more names',
+                        style: GoogleFonts.poppins(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Make sure your entered only first name",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                        fontSize: 10.sp,
+                        color: Colors.grey
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ) : Container(
           child: Column(
             children: [
               Align(
